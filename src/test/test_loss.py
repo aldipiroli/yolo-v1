c6d2ec3b31317f500dabf7f5b5f6945e7ddb3512
Your Name <you@example.com>
2021-08-01 13:35:58 +0200

    [ADD] Added working batch loss

--- src/test/test_loss.py
+++ src/test/test_loss.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import random
+import torch
 from random import randrange
 import matplotlib.pyplot as plt
 
@@ -10,11 +11,13 @@ from src.utils.plot import plot_boxes
 
 
 class TestLoss(unittest.TestCase):
-    def make_gt(self, S=7, C=20, W=448, H=448, manual_input=False):
-        counter = 0
-        gt = np.zeros((S, S, 24))
-        
-        if not manual_input:
+    def make_gt(self, batch_size=2, S=7, C=20, W=448, H=448, manual_input=False):
+        gts = []
+
+        for _ in range(batch_size):
+            counter = 0
+            gt = np.zeros((S, S, 24))
+
             for i in range(S):
                 for j in range(S):
 
@@ -30,63 +33,55 @@ class TestLoss(unittest.TestCase):
                             rnd_cls = randrange(C)
                             gt[i, j, 4 + rnd_cls] = 1
                             counter += 1
+            gts.append(gt)
 
-        else:
-            gt[2, 2, :4] = [0, 0, 50, 100]
-            rnd_cls = 1
-            gt[2, 2, 4 + rnd_cls] = 1
-            
-        return gt, counter
+        return np.array(gts)
 
-    def make_pr(self, gt, B=2, C=20, err=5, manual_input=False):
-        S = gt.shape[0]
-        pr = np.zeros((S, S, B * 5 + C))
-        counter = 0
+    def make_pr(self, gts, S=7, B=2, C=20, err=15, manual_input=False):
+        prs = []
+        for k in range(gts.shape[0]):
+            pr = np.zeros((S, S, B * 5 + C))
+            gt = gts[k, :]
 
-        if not manual_input:
             for i in range(S):
                 for j in range(S):
                     if gt[i, j, 4:].any() != 0:
                         # Box 1:
-                        pr[i, j, :4] = gt[i, j, :4] + random.uniform(-err, err)
-                        pr[i, j, 4]  = 0.33
+                        pr[i, j, :2] = np.clip(gt[i, j, :2] + random.uniform(-err, err), 0.1, 1)
+                        pr[i, j, 2:4] = np.clip(gt[i, j, 2:4] + random.uniform(-err, err), 1, err)
+                        pr[i, j, 4] = 0.33
 
                         # Box 2:
                         pr[i, j, 5:9] = gt[i, j, :4] + random.uniform(-err, err)
-                        pr[i, j, 9]  = 0.85
+                        pr[i, j, 9] = 0.85
 
                         # Random class
                         pr[i, j, 10:] = 0
                         rnd_cls = randrange(C)
                         pr[i, j, 10 + rnd_cls] = 1
+            prs.append(pr)
 
-                        counter += 1
-
-        else:
-            pr[2, 2, :5] = [0, 0, 70, 20, 0.88]
-            pr[2, 2, 5:10] = [0, 0, 20, 60, 0.97]
-
-        return pr, counter
+        return np.array(prs)
 
     def test_loss(self):
         manual_input = False
-        gt, sum_gt = self.make_gt(manual_input=manual_input)
-        pr, sum_pr = self.make_pr(gt, manual_input=manual_input)
-        assert sum_gt == sum_pr,  "#GT Boxes != #PR Boxes"
+        gt = self.make_gt(manual_input=manual_input)
+        pr = self.make_pr(gt, manual_input=manual_input)
 
         compute_loss(gt, pr)
-        
-        # Plot GT
-        fig, ax = plot_boxes(gt, color="red")
 
-        # Plot Box 1
-        pred_box = np.concatenate((pr[:, :, :4], pr[:, :, 10:]), axis=2)
-        fig, ax = plot_boxes(pred_box, color="lime", fig=fig, ax=ax)
+        # # Plot GT
+        # batch_idx = 0
+        # fig, ax = plot_boxes(gt[batch_idx, :], color="red")
 
-        # Plot Box 2
-        pred_box = np.concatenate((pr[:, :, 5:9], pr[:, :, 10:]), axis=2)
-        fig, ax = plot_boxes(pred_box, color="aqua", fig=fig, ax=ax)
-        plt.show()
+        # # Plot Box 1
+        # pred_box = np.concatenate((pr[batch_idx, :, :, :4], pr[batch_idx, :, :, 10:]), axis=2)
+        # fig, ax = plot_boxes(pred_box, color="lime", fig=fig, ax=ax)
+
+        # # Plot Box 2
+        # pred_box = np.concatenate((pr[batch_idx, :, :, 5:9], pr[batch_idx, :, :, 10:]), axis=2)
+        # fig, ax = plot_boxes(pred_box, color="aqua", fig=fig, ax=ax)
+        # plt.show()
 
 
 if __name__ == "__main__":
